4e1053b82b17b538b2c0274f25dc246e88b7339d
YURAKUSHNIRUK <yurakushniruk@ukr.net>
2021-11-29 21:14:01 +0100

    Created restrictedWords custom validator

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,7 +5,8 @@ import {
     EventDetailsComponent,
     CreateEventComponent, 
     EventRouteActivator,
-    EventsListResolver
+    EventsListResolver,
+    CreateSessionComponent
 } from './events/index'
 
 import { Error404Component } from "./errors/404.component";
@@ -13,7 +14,8 @@ import { Error404Component } from "./errors/404.component";
 export const appRoutes:Routes = [
     { path:'events/new', component: CreateEventComponent, canDeactivate: ["canDeactivateCreateEvent"] },
     { path:'events', component: EventsListComponent, resolve: {events:EventsListResolver} },
-    { path:'events/:id', component: EventDetailsComponent, canActivate:[EventRouteActivator] },
+    { path:'events/:id', component: EventDetailsComponent, canActivate:[EventRouteActivator] },    
+    { path:'events/session/new', component: CreateSessionComponent },
     { path:'404', component: Error404Component},
     { path:'', redirectTo:'/events', pathMatch:'full'},
     { 
