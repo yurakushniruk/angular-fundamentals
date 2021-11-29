e42d7854ace39c4c4c850e3ff4f480792513fcd3
YURAKUSHNIRUK <yurakushniruk@ukr.net>
2021-11-29 16:36:56 +0100

    Created a reactive form with validation

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,10 +1,18 @@
 import { Component, OnInit } from '@angular/core'
-import { FormControl, FormGroup } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
 import { AuthService } from './auth.service'
 
 @Component({
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
+  styles: [`
+    em { float:right; color:#E05C65; padding-left:10px; }
+    .error input { background-color:#E3C3C5; }
+    .error ::webkit-input-placeholder { color:#999; }
+    .error ::moz-placeholder { color:#999; }
+    .error :-moz-placeholder { color:#999; }
+    .error ::ms-input-placeholder { color:#999; }
+  `]
 })
 export class ProfileComponent implements OnInit {
   profileForm:FormGroup
@@ -14,8 +22,8 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit() {
-    let firstName = new FormControl(this.authService.currentUser.firstName)
-    let lastName = new FormControl(this.authService.currentUser.lastName)
+    let firstName = new FormControl(this.authService.currentUser.firstName, Validators.required)
+    let lastName = new FormControl(this.authService.currentUser.lastName, Validators.required)
     this.profileForm = new FormGroup({
       firstName: firstName,
       lastName: lastName
@@ -23,8 +31,10 @@ export class ProfileComponent implements OnInit {
   }
 
   saveProfile(formValues) {
-    this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
-    this.router.navigate(['events'])
+    if(this.profileForm.valid) {
+      this.authService.updateCurrentUser(formValues.firstName, formValues.lastName)
+      this.router.navigate(['events'])
+    }
   }
 
   cancel() {
