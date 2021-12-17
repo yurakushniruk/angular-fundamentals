260a87ff149469c3f8cb1bae987aa6de0d17a651
YURAKUSHNIRUK <yurakushniruk@ukr.net>
2021-12-17 20:08:26 +0100

    Added pipes for displaying data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,8 @@ import {
   EventRouteActivator,
   EventsListResolver,
   CreateSessionComponent,
-  SessionListComponent
+  SessionListComponent,
+  DurationPipe
 } from './events/index'
 
 import { ToastrService } from 'src/app/common/toastr.service'
@@ -41,7 +42,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     Error404Component,
     CreateSessionComponent,
     SessionListComponent,
-    CollapsibleWellComponent
+    CollapsibleWellComponent,
+    DurationPipe
   ],  
   providers: [
     EventService, 
