1d6fd613fff4308433423c08b1ae554eac20d2f6
YURAKUSHNIRUK <yurakushniruk@ukr.net>
2021-12-27 17:17:15 +0100

    Added sessions filter by level

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -15,6 +15,7 @@ import { IEvent, ISession } from "../shared/index";
 export class EventDetailsComponent {
     event:IEvent
     addMode:boolean
+    filterBy: string = 'all'
 
     constructor(private eventService:EventService, private route:ActivatedRoute){
         
--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { ISession } from "../shared/index";
 
 @Component({
@@ -6,6 +6,24 @@ import { ISession } from "../shared/index";
     templateUrl: 'session-list.component.html'
 })
 
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
     @Input() sessions:ISession[]
+    @Input() filterBy: string
+    visibleSessions: ISession[] = []
+
+    ngOnChanges() {
+        if(this.sessions) {
+            this.filterSessions(this.filterBy)
+        }        
+    }
+
+    filterSessions(filter) {
+        if(filter === 'all') {
+            this.visibleSessions = this.sessions.slice(0) //creates a complete duplicate of the array with the same elements
+        } else {
+            this.visibleSessions = this.sessions.filter(session => { 
+                return session.level.toLocaleLowerCase() === filter
+            })
+        }
+    }
 }
