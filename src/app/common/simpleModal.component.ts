36bc363e39567b204681bc46802cebb80b743035
YURAKUSHNIRUK <yurakushniruk@ukr.net>
2022-01-17 20:57:01 +0100

    Fixing and improving modal window with search results

--- src/app/common/simpleModal.component.ts
+++ src/app/common/simpleModal.component.ts
@@ -1,16 +1,18 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, ViewChild, ElementRef, Inject } from "@angular/core";
+import { $ } from "protractor";
+import { JQ_TOKEN } from "./jQuery.service";
 
 @Component({
     selector: 'simple-modal',
     template: `
-        <div id="simple-modal" class="modal fade" tabindex="-1">
+        <div id="{{elementId}}" #modalcontainer class="modal fade" tabindex="-1">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
                         <h4 class="modal-title">{{title}}</h4>
                     </div>
-                    <div class="modal-body">
+                    <div class="modal-body" (click)="closeModal()">
                         <ng-content></ng-content>
                     </div>
                 </div>
@@ -24,4 +26,18 @@ import { Component, Input } from "@angular/core";
 
 export class SimpleModalComponent {
     @Input() title: string
+    @Input() elementId: string
+    @Input() closeOnBodyClick: string
+    @ViewChild('modalcontainer') containerEl: ElementRef
+
+    constructor(@Inject(JQ_TOKEN) private $:any) {
+
+    }
+
+    closeModal() {
+        if(this.closeOnBodyClick.toLocaleLowerCase() === 'true') {
+            this.$(this.containerEl.nativeElement).modal('hide')
+        }
+        
+    }
 }
